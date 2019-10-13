b85befcc1aa0b5471faf729f4e4bd3a54e0f8220
Ermir Beqiraj <ermirbeqiraj@hotmail.com>
2019-10-13 21:07:32 +0200

    generic repository api client

---
+++ GenericRepositoryPattern.ApiClient/Controllers/ServiceController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using GenericRepositoryPattern.DAL;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GenericRepositoryPattern.ApiClient.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServiceController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ServiceController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_unitOfWork.ServiceRepository.Get());
+        }
+
+        [HttpGet("{id}", Name = "Get")]
+        public IActionResult Get(Guid id)
+        {
+            var dbItem = _unitOfWork.ServiceRepository.Get(id);
+            return Ok(dbItem);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Service model)
+        {
+            if (ModelState.IsValid)
+            {
+                var dbCar = _unitOfWork.CarRepository.Get(model.CarId);
+
+                if (dbCar == null)
+                    return NotFound();
+
+                dbCar.LastService = DateTime.Now;
+                _unitOfWork.CarRepository.Update(dbCar);
+                _unitOfWork.ServiceRepository.Insert(model);
+
+                _unitOfWork.Commit();
+
+                return Ok();
+            }
+
+            return BadRequest(model);
+        }
+    }
+}
